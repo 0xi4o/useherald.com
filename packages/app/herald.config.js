545997d7394a2cc56824cacc2203a797c2be7539
Ilango <rajagopalilango@gmail.com>
2021-07-25 16:05:43 +0530

    Add a configuration file for global settings

---
+++ packages/app/herald.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: 'Herald',
+	footerLinks: [
+		{ label: 'Home', link: '#' },
+		{ label: 'About', link: '#' },
+		{ label: 'Blog', link: '#' },
+		{ label: 'Contact', link: '#' },
+	],
+	logoIcon: '/logo.png',
+	social: {
+		twitter: '#',
+	},
+};
