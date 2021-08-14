6f04366cbb77ffff786ee11f1267652354f4326f
Ilango <rajagopalilango@gmail.com>
2021-08-14 12:31:11 +0530

    Add coming soon message to public page and widget

--- packages/app/pages/admin/settings/public-page.js
+++ packages/app/pages/admin/settings/public-page.js
@@ -1,7 +1,24 @@
+import { Box, useColorModeValue } from '@chakra-ui/react';
 import SettingsLayout from '../../../components/layouts/Settings';
 
 function PublicPage() {
-	return <SettingsLayout>PublicPage</SettingsLayout>;
+	return (
+		<SettingsLayout>
+			<Box
+				w='full'
+				h={48}
+				d='flex'
+				alignItems='center'
+				justifyContent='center'
+				borderWidth={2}
+				borderStyle='solid'
+				borderColor={useColorModeValue('gray.400', 'gray.700')}
+				borderRadius='lg'
+			>
+				Coming soon!
+			</Box>
+		</SettingsLayout>
+	);
 }
 
 export default PublicPage;
