54d52a33ea27e530a5fc4e9bc568a32bc32b8b21
Ilango <rajagopalilango@gmail.com>
2021-09-10 20:01:47 +0530

    Create fresh widget package that builds without error

---
+++ packages/widget/src/app.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    <>
+      <p>Hello Vite + Preact!</p>
+      <p>
+        <a
+          className="link"
+          href="https://preactjs.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Preact
+        </a>
+      </p>
+    </>
+  )
+}
