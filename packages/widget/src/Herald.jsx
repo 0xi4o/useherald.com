194e3f39eee553e06385abf1e4ff2c06ca341207
Ilango <rajagopalilango@gmail.com>
2021-09-11 13:18:50 +0530

    Update widget popover panel styles

--- packages/widget/src/Herald.jsx
+++ packages/widget/src/Herald.jsx
@@ -1,7 +1,7 @@
 import { Popover, Transition } from '@headlessui/react';
 import React, { Fragment, useState } from 'react';
 
-function Herald() {
+function Herald(props) {
 	const [count, setCount] = useState(0);
 
 	const solutions = [
@@ -26,7 +26,7 @@ function Herald() {
 	];
 
 	return (
-		<div className='w-full max-w-sm px-4 fixed top-16'>
+		<div className='w-auto'>
 			<Popover className='relative'>
 				{({ open }) => (
 					<>
@@ -35,7 +35,7 @@ function Herald() {
 								open ? '' : 'text-opacity-90'
 							} text-white group bg-orange-700 px-3 py-2 rounded-md inline-flex items-center text-base font-medium hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
 						>
-							<span>Solutions</span>
+							{props.icon || props.text}
 						</Popover.Button>
 						<Transition
 							as={Fragment}
@@ -46,14 +46,14 @@ function Herald() {
 							leaveFrom='opacity-100 translate-y-0'
 							leaveTo='opacity-0 translate-y-1'
 						>
-							<Popover.Panel className='absolute z-10 w-screen max-w-sm px-4 mt-3 transform -translate-x-1/2 left-1/2 sm:px-0 lg:max-w-3xl'>
+							<Popover.Panel className='absolute z-10 w-72 px-4 mt-3 transform -translate-x-3/4 left-3/4 sm:px-0'>
 								<div className='overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5'>
-									<div className='relative grid gap-8 bg-white p-7 lg:grid-cols-2'>
+									<div className='relative grid gap-0 divide-y divide-gray-100 bg-white p-0'>
 										{solutions.map((item) => (
 											<a
 												key={item.name}
 												href={item.href}
-												className='flex items-center p-2 -m-3 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50'
+												className='flex items-center p-4 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50'
 											>
 												<div className='flex items-center justify-center flex-shrink-0 w-10 h-10 text-white sm:h-12 sm:w-12'>
 													<item.icon aria-hidden='true' />
@@ -69,20 +69,12 @@ function Herald() {
 											</a>
 										))}
 									</div>
-									<div className='p-4 bg-gray-50'>
+									<div className='p-0 bg-gray-50'>
 										<a
-											href='##'
-											className='flow-root px-2 py-2 transition duration-150 ease-in-out rounded-md hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50'
+											href='https://useherald.com'
+											className='text-xs flow-root px-2 py-2 text-center transition duration-150 ease-in-out rounded-md hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50'
 										>
-											<span className='flex items-center'>
-												<span className='text-sm font-medium text-gray-900'>
-													Documentation
-												</span>
-											</span>
-											<span className='block text-sm text-gray-500'>
-												Start integrating products and
-												tools
-											</span>
+											Powered by Herald
 										</a>
 									</div>
 								</div>
