9e10ec8e34a62b315211f2635dcb4ac3e17a5e19
Ilango <rajagopalilango@gmail.com>
2021-09-12 09:09:24 +0530

    Add next-plugin-transpile and related config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
-const { withDokz } = require('dokz/dist/plugin');
+const withPlugins = require('next-compose-plugins');
+const withTm = require('next-transpile-modules')(['@herald/widget']);
+const dokz = require('dokz/dist/plugin');
 
-module.exports = withDokz({
-	images: {
-		domains: ['useherald.com'],
-	},
-	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-	reactStrictMode: true,
-});
+module.exports = withPlugins([
+	[
+		dokz,
+		{
+			images: {
+				domains: ['useherald.com'],
+			},
+			pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+			reactStrictMode: true,
+		},
+	],
+	withTm,
+]);
