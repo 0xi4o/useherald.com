fb9012525d05ef18f8817818113fe023381e86a5
Ilango <rajagopalilango@gmail.com>
2021-09-27 10:03:58 +0530

    Update next.config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
 const withPlugins = require('next-compose-plugins');
 const withTm = require('next-transpile-modules')(['@herald/widget']);
-const dokz = require('dokz/dist/plugin');
+const { withDokz } = require('dokz/dist/plugin');
 
-module.exports = withPlugins([
-	[
-		dokz,
-		{
-			images: {
-				domains: ['useherald.com'],
-			},
-			pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-			reactStrictMode: true,
-		},
-	],
-	withTm,
-]);
+module.exports = withPlugins([withDokz, withTm], {
+	images: {
+		domains: ['useherald.com'],
+	},
+	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+	reactStrictMode: true,
+});
+
+// module.exports =
