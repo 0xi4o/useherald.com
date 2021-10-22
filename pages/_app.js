4dc6eee56618c93f078d79798d84487fee2a3cd9
Ilango <rajagopalilango@gmail.com>
2021-10-22 16:47:46 +0530

    We have confirmation. I AM an idiot.

--- pages/_app.js
+++ pages/_app.js
@@ -16,168 +16,180 @@ import '@useherald/react-widget/dist/style.css';
 import { pageview } from '../lib/gtag';
 
 function MyApp({ Component, pageProps }) {
-	const { events, pathname } = useRouter();
+	const router = useRouter();
 
 	useEffect(() => {
 		if (window) {
 			const handleRouteChange = (url) => {
 				pageview(url);
 			};
-			events.on('routeChangeComplete', handleRouteChange);
+			router.events.on('routeChangeComplete', handleRouteChange);
 			return () => {
-				events.off('routeChangeComplete', handleRouteChange);
+				router.events.off('routeChangeComplete', handleRouteChange);
 			};
 		}
-	}, [events]);
+	}, [router.events]);
 
-	if (pathname.startsWith('/docs')) {
+	if (router.pathname.startsWith('/docs')) {
 		return (
-			<ChakraProvider resetCSS theme={theme}>
-				<Head>
-					<title>
-						Herald Docs | Updates-as-a-service for SaaS products |
-						Changelog, Banners and Waitlist Management
-					</title>
-					<Script
-						strategy='afterInteractive'
-						src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-					/>
-					<Script
-						id='google-analytics'
-						strategy='afterInteractive'
-						dangerouslySetInnerHTML={{
-							__html: `
-								window.dataLayer = window.dataLayer || [];
-								function gtag(){dataLayer.push(arguments);}
-								gtag('js', new Date());
-								
-								gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-									page_path: window.location.pathname,
-								});
-							`,
-						}}
-					/>
-				</Head>
-				<DokzProvider
-					docsRootPath='pages/docs'
-					headerItems={[<ColorModeSwitch key={1} />]}
-					headerLogo={
-						<Link href='/'>
-							<HStack cursor='pointer'>
-								<Image
-									src='/logo.png'
-									width={40}
-									height={40}
-									alt='Herald Logo'
-								/>
-								<chakra.span fontSize='xl' fontWeight='bold'>
-									Herald
-								</chakra.span>
-							</HStack>
-						</Link>
-					}
-					sidebarOrdering={{
-						docs: {
-							about: true,
-							'getting-started': {
-								introduction: true,
-								configuration: true,
-							},
-						},
+			<>
+				<Script
+					strategy='afterInteractive'
+					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+				/>
+				<Script
+					id='google-analytics'
+					strategy='afterInteractive'
+					dangerouslySetInnerHTML={{
+						__html: `
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+							
+							gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+								page_path: window.location.pathname,
+							});
+						`,
 					}}
-				>
-					<Component {...pageProps} />
-				</DokzProvider>
-			</ChakraProvider>
-		);
-	} else if (pathname.startsWith('/blog')) {
-		return (
-			<ChakraProvider resetCSS theme={theme}>
-				<Head>
-					<title>
-						Herald Blog | Updates-as-a-service for SaaS products |
-						Changelog, Banners and Waitlist Management
-					</title>
-					<Script
-						strategy='afterInteractive'
-						src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-					/>
-					<Script
-						id='google-analytics'
-						strategy='afterInteractive'
-						dangerouslySetInnerHTML={{
-							__html: `
-								window.dataLayer = window.dataLayer || [];
-								function gtag(){dataLayer.push(arguments);}
-								gtag('js', new Date());
-								
-								gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-									page_path: window.location.pathname,
-								});
-							`,
+				/>
+				<ChakraProvider resetCSS theme={theme}>
+					<Head>
+						<title>
+							Herald Docs | Updates-as-a-service for SaaS products
+							| Changelog, Banners and Waitlist Management
+						</title>
+					</Head>
+					<DokzProvider
+						docsRootPath='pages/docs'
+						headerItems={[<ColorModeSwitch key={1} />]}
+						headerLogo={
+							<Link href='/'>
+								<HStack cursor='pointer'>
+									<Image
+										src='/logo.png'
+										width={40}
+										height={40}
+										alt='Herald Logo'
+									/>
+									<chakra.span
+										fontSize='xl'
+										fontWeight='bold'
+									>
+										Herald
+									</chakra.span>
+								</HStack>
+							</Link>
+						}
+						sidebarOrdering={{
+							docs: {
+								about: true,
+								'getting-started': {
+									introduction: true,
+									configuration: true,
+								},
+							},
 						}}
-					/>
-				</Head>
-				<DokzBlogProvider
-					blogRootPath='pages/blog'
-					headerItems={[<ColorModeSwitch key={1} />]}
-					headerLogo={
-						<Link href='/'>
-							<HStack cursor='pointer'>
-								<Image
-									src='/logo.png'
-									width={40}
-									height={40}
-									alt='Herald Logo'
-								/>
-								<chakra.span fontSize='xl' fontWeight='bold'>
-									Herald
-								</chakra.span>
-							</HStack>
-						</Link>
-					}
-				>
-					<Component {...pageProps} />
-				</DokzBlogProvider>
-			</ChakraProvider>
+					>
+						<Component {...pageProps} />
+					</DokzProvider>
+				</ChakraProvider>
+			</>
+		);
+	} else if (router.pathname.startsWith('/blog')) {
+		return (
+			<>
+				<Script
+					strategy='afterInteractive'
+					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+				/>
+				<Script
+					id='google-analytics'
+					strategy='afterInteractive'
+					dangerouslySetInnerHTML={{
+						__html: `
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+							
+							gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+								page_path: window.location.pathname,
+							});
+						`,
+					}}
+				/>
+				<ChakraProvider resetCSS theme={theme}>
+					<Head>
+						<title>
+							Herald Blog | Updates-as-a-service for SaaS products
+							| Changelog, Banners and Waitlist Management
+						</title>
+					</Head>
+					<DokzBlogProvider
+						blogRootPath='pages/blog'
+						headerItems={[<ColorModeSwitch key={1} />]}
+						headerLogo={
+							<Link href='/'>
+								<HStack cursor='pointer'>
+									<Image
+										src='/logo.png'
+										width={40}
+										height={40}
+										alt='Herald Logo'
+									/>
+									<chakra.span
+										fontSize='xl'
+										fontWeight='bold'
+									>
+										Herald
+									</chakra.span>
+								</HStack>
+							</Link>
+						}
+					>
+						<Component {...pageProps} />
+					</DokzBlogProvider>
+				</ChakraProvider>
+			</>
 		);
 	}
 
 	return (
-		<ChakraProvider resetCSS theme={theme}>
-			<ColorModeProvider
-				options={{
-					useSystemColorMode: true,
+		<>
+			<Script
+				strategy='afterInteractive'
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+			/>
+			<Script
+				id='google-analytics'
+				strategy='afterInteractive'
+				dangerouslySetInnerHTML={{
+					__html: `
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						
+						gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+							page_path: window.location.pathname,
+						});
+					`,
 				}}
-			>
-				<Head>
-					<title>
-						Herald | Updates-as-a-service for SaaS products |
-						Changelog, Banners and Waitlist Management
-					</title>
-					<Script
-						strategy='afterInteractive'
-						src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-					/>
-					<Script
-						id='google-analytics'
-						strategy='afterInteractive'
-						dangerouslySetInnerHTML={{
-							__html: `
-								window.dataLayer = window.dataLayer || [];
-								function gtag(){dataLayer.push(arguments);}
-								gtag('js', new Date());
-								
-								gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-									page_path: window.location.pathname,
-								});
-							`,
-						}}
-					/>
-				</Head>
-				<Component {...pageProps} />
-			</ColorModeProvider>
-		</ChakraProvider>
+			/>
+			<ChakraProvider resetCSS theme={theme}>
+				<ColorModeProvider
+					options={{
+						useSystemColorMode: true,
+					}}
+				>
+					<Head>
+						<title>
+							Herald | Updates-as-a-service for SaaS products |
+							Changelog, Banners and Waitlist Management
+						</title>
+					</Head>
+					<Component {...pageProps} />
+				</ColorModeProvider>
+			</ChakraProvider>
+		</>
 	);
 }
 
