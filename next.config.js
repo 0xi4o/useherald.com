569af0dcd46f949481b49f7e73c0f865979570af
Ilango <rajagopalilango@gmail.com>
2021-11-02 20:57:10 +0530

    👌 IMPROVE: Update security headers

--- next.config.js
+++ next.config.js
@@ -1,32 +1,37 @@
+const { createSecureHeaders } = require('next-secure-headers');
 const withPlugins = require('next-compose-plugins');
 const withTm = require('next-transpile-modules')(['@useherald/react-widget']);
 const { withDokz } = require('dokz/dist/plugin');
 
-const securityHeaders = [
-	{
-		key: 'X-Frame-Options',
-		value: 'SAMEORIGIN',
-	},
-	{
-		key: 'Permissions-Policy',
-		value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
-	},
-	{
-		key: 'X-Content-Type-Options',
-		value: 'nosniff',
-	},
-	{
-		key: 'Referrer-Policy',
-		value: 'origin-when-cross-origin',
-	},
-];
+// const securityHeaders = [
+// 	{
+// 		key: 'X-Frame-Options',
+// 		value: 'SAMEORIGIN'
+// 	},
+// 	{
+// 		key: 'Permissions-Policy',
+// 		value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
+// 	},
+// 	{
+// 		key: 'X-Content-Type-Options',
+// 		value: 'nosniff'
+// 	},
+// 	{
+// 		key: 'Referrer-Policy',
+// 		value: 'origin-when-cross-origin'
+// 	},
+// 	{
+// 		key: 'Content-Security-Policy',
+// 		value: ''
+// 	}
+// ];
 
 module.exports = withPlugins([withDokz, withTm], {
 	async headers() {
 		return [
 			{
 				source: '/(.*)',
-				headers: securityHeaders,
+				headers: createSecureHeaders(),
 			},
 		];
 	},
